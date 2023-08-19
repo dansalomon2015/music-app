a1f089aa61a9b2231db60c10cc0ba2558593cdbc
Salomon Nghukam <dsalomonj@gmail.com>
2023-08-19 14:35:01 +0100

    setting up firebase for backend

--- backend-node/functions/src/index.ts
+++ backend-node/functions/src/index.ts
@@ -1,13 +1,34 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+import { NextFunction, Request, Response } from "express";
 import { globalErrorHandler } from "./middleware";
 import SearchRoutes from "./routes/SearchRoutes";
 import ArtistRoutes from "./routes/ArtistRoutes";
 import { ApiResponse } from "./utils";
 import { HTTP_RESPONSE_CODES } from "./types";
 
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+import express = require("express");
+import cors = require("cors");
+
+const PORT = 5000;
 const app = express();
 
 // Use the cors middleware
@@ -43,3 +64,5 @@ app.use(globalErrorHandler);
 app.listen(PORT, () => {
     console.log(`Start listening on port ${PORT} `);
 });
+
+export const webApp = onRequest(app);

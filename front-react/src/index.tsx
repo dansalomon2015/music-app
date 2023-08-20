0270fb042c925b79525b6de5cb563f8520f9a5a7
Salomon Nghukam <dsalomonj@gmail.com>
2023-08-20 05:47:05 +0100

    refactory

--- front-react/src/index.tsx
+++ front-react/src/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ArtistDetails, Home } from "pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/infos/:artistId" element={<ArtistDetails />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
---
+++ front-react/src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import { useMemo, useState } from "react";
+import { useSearch } from "hooks";
+import { SearResultItem, SearchInput, SearchResultPlaceholder } from "components";
+
+export const Home = () => {
+    const [searchText, setSearchText] = useState("");
+    const { result, loading } = useSearch({ queryParam: searchText });
+
+    const bodyComponent = useMemo(() => {
+        if (loading) return <SearchResultPlaceholder count={10} />;
+        if (!!result.length)
+            return (
+                <>
+                    <p className="ml-4 mt-4 sm:mt-1">Search Results : </p>
+                    <div className="mt-8 grid overflow-hidden px-2 md:px-4 grid-cols-2 grid-rows-3 gap-6 sm:grid-cols-4 sm:grid-rows-3 sm:gap-4 md:grid-cols-4 md:grid-rows-3 md:gap-3.5 lg:grid-cols-6 lg:grid-rows-3">
+                        {result.map((item, i) => {
+                            return <SearResultItem item={item} key={i} />;
+                        })}
+                    </div>
+                </>
+            );
+        if (result.length === 0 && !loading)
+            return (
+                <div className="flex justify-center items-center font-semibold text-2xl">
+                    {!!searchText.length ? "No data found !" : "Search your preferred songs "}
+                </div>
+            );
+    }, [result, loading, searchText]);
+    return (
+        <div>
+            <div className="py-3 px-3 md:px-4 md:py-5  bg-orange-500 shadow-md">
+                <h1 className="text-white font-bold text-xl">Music App</h1>
+            </div>
+
+            <SearchInput value={searchText} onValueChange={setSearchText} loading={loading} />
+
+            {bodyComponent}
+        </div>
+    );
+};
